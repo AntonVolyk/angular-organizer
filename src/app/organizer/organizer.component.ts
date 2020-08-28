8c9ba9b5af256be522dd0e74a94187309f81db34
volik <volik.anton@gmail.com>
2020-08-28 13:07:16 +0300

    create, read, update firebase data

--- src/app/organizer/organizer.component.ts
+++ src/app/organizer/organizer.component.ts
@@ -2,6 +2,7 @@ import { TaskService, Task } from './../shared/tasks.service';
 import { DateService } from './../shared/date.service';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-organizer',
@@ -10,25 +11,35 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class OrganizerComponent implements OnInit {
   form: FormGroup;
+  tasks: Task[] = [];
 
   constructor(public readonly dateService: DateService, private readonly taskService: TaskService) { }
 
   ngOnInit(): void {
+    this.dateService.date.pipe(
+        switchMap(value => this.taskService.load(value))
+      ).subscribe(tasks => this.tasks = tasks);
     this.form = new FormGroup({
       title: new FormControl('', Validators.required)
     });
   }
 
   submit(): void {
-    const {title} = this.form.value;
+    const { title } = this.form.value;
     const task: Task = {
       title,
       date: this.dateService.date.value.format('DD-MM-YYYY')
     };
     this.taskService.create(task).subscribe(res => {
+      this.tasks.push(res);
       this.form.reset();
       console.log(res);
     }, console.error);
   }
 
+  remove(task: Task): void {
+    this.taskService.remove(task).subscribe(() => {
+      this.tasks = this.tasks.filter(t => t.id !== task.id);
+    }, console.error);
+  }
 }
--- src/app/shared/tasks.service.ts
+++ src/app/shared/tasks.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+import * as moment from 'moment';
 
 export interface Task {
     id?: string;
@@ -30,4 +31,20 @@ export class TaskService {
             }));
     }
 
+    load(date: moment.Moment): Observable<Task[]> {
+        return this.http
+            .get<Task[]>(`${TaskService.url}/${date.format('DD-MM-YYYY')}.json`)
+            .pipe(map(tasks => {
+                if (!tasks){
+                    return [];
+                }
+                console.log(`Firebase response: ${tasks}`);
+                return Object.keys(tasks).map(key => ({...tasks[key], id: key}));
+            }));
+    }
+
+    remove(task: Task): Observable<void> {
+        return this.http
+            .delete<void>(`${TaskService.url}/${task.date}/${task.id}.json`);
+    }
 }
